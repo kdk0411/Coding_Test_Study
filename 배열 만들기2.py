8419d541960945fdce6fa5f6777b211e40a4c5f7
Administrator <Administrator@MY-20220518MUKK>
2023-10-13 17:55:20 +0900

    Coding_Test_Prac

--- 배열 만들기2.py
+++ 배열 만들기2.py
@@ -1,7 +1,7 @@
 def solution(l, r):
   answer = []
   for i in range(l, r + 1):
-    if all(num in ['0', '5'] for num in str(i)):
+    if all(j in ['0', '5'] for j in str(i)):
       answer.append(i)
   if len(answer) == 0:
     answer.append(-1)

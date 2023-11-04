5c82aa476fad03886564ddbd1ca9d92834f6e50d
kdk0411 <wnsldjqja@gmail.com>
2023-11-04 22:11:19 +0900

    coding_test_study

---
+++ LV.02/연속 부분 수열 합의 개수.py
@@ -0,0 +1,8 @@
+def solution(elements):
+  cycle = elements + elements
+  print(cycle)
+  s = set()
+  for i in range(len(elements)):
+    for j in range(len(elements)):
+      s.add(sum(cycle[i:i + j]))
+  return len(s)
